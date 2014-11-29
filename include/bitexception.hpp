dfbc92a30647ddaf8b9d63c86b5c213e63054aef
Oz <rik20@live.it>
2014-11-29 08:13:27 +0100

    Updated BitException with support to UString strings

--- include/bitexception.hpp
+++ include/bitexception.hpp
@@ -4,10 +4,19 @@
 #include <iostream>
 #include <exception>
 
+#include "Common/MyString.h"
+
 namespace Bit7z {
     class BitException : public std::runtime_error {
         public:
             BitException( const std::string& );
+            BitException(UString & );
+
+            const char *what() const;
+
+        private:
+            std::string unicodeMessage;
+            bool isUnicode;
     };
 }
 
--- src/bitexception.cpp
+++ src/bitexception.cpp
@@ -2,4 +2,18 @@
 
 using namespace Bit7z;
 
-BitException::BitException( const std::string&  message ) : std::runtime_error( message ) {}
+BitException::BitException( const std::string&  message ) : std::runtime_error( message ),
+    isUnicode( false ) {}
+
+BitException::BitException( UString& message ) : std::runtime_error( "error" ),
+    isUnicode( true ) {
+    const std::wstring msg( message.GetBuffer( 0 ) );
+    unicodeMessage = std::string( msg.begin(), msg.end() );
+}
+
+const char* BitException::what() const {
+    if ( isUnicode )
+        return unicodeMessage.c_str();
+
+    return runtime_error::what();
+}
