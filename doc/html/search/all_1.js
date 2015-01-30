84abce923ef65595b31a1b3fedbb0d26dbb475fd
Oz <rik20@live.it>
2015-01-30 11:02:00 +0100

    Added doxygen documentation

---
+++ doc/html/search/all_1.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['compress',['compress',['../classbit7z_1_1_bit_compressor.html#a85d49eb05843a51f56a5695a291f8b08',1,'bit7z::BitCompressor']]],
+  ['compressdirectory',['compressDirectory',['../classbit7z_1_1_bit_compressor.html#a3d168518cbbeffe87eb2eb0fd604d44e',1,'bit7z::BitCompressor']]],
+  ['compressfile',['compressFile',['../classbit7z_1_1_bit_compressor.html#a46929a086b30f0d4a1f77562f4bd5b1c',1,'bit7z::BitCompressor']]],
+  ['compressfiles',['compressFiles',['../classbit7z_1_1_bit_compressor.html#a5f45d061e0480dc242d75e4d889a6edd',1,'bit7z::BitCompressor::compressFiles(const vector&lt; wstring &gt; &amp;in_files, const wstring &amp;out_archive) const '],['../classbit7z_1_1_bit_compressor.html#af11b8126aa0402cfdb97da36e5c0a189',1,'bit7z::BitCompressor::compressFiles(const wstring &amp;in_dir, const wstring &amp;out_archive, const wstring &amp;filter=L&quot;*&quot;, bool recursive=true) const ']]],
+  ['compressionformat',['compressionFormat',['../classbit7z_1_1_bit_compressor.html#ae8da755531baed278daa7eaf6f631137',1,'bit7z::BitCompressor']]]
+];
