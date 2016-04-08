b650ab7c9ee043799a79d91848b9f8aaf89c8393
Oz <rik20@live.it>
2016-04-08 21:14:52 +0200

    Remove doc from repository

--- doc/html/search/functions_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['bit7zlibrary',['Bit7zLibrary',['../classbit7z_1_1_bit7z_library.html#ae7532291e76d568836e54f03c5a9b47b',1,'bit7z::Bit7zLibrary']]],
-  ['bitcompressor',['BitCompressor',['../classbit7z_1_1_bit_compressor.html#aa2f45fb0543b68800a8ba4f2184e8d67',1,'bit7z::BitCompressor']]],
-  ['bitexception',['BitException',['../classbit7z_1_1_bit_exception.html#a711d63e991fb8e7525bcf1dc34dd5799',1,'bit7z::BitException::BitException(const std::string &amp;message)'],['../classbit7z_1_1_bit_exception.html#aae4b285f6ca424fbfc7285468f93631c',1,'bit7z::BitException::BitException(const std::wstring &amp;message)']]],
-  ['bitextractor',['BitExtractor',['../classbit7z_1_1_bit_extractor.html#abccf328757cc325e342c068696649c8c',1,'bit7z::BitExtractor']]]
-];
