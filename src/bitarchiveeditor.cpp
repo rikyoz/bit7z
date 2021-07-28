1582967c77db661f6d362db8998bcbb8499c605f
Oz <rik20@live.it>
2021-07-28 14:45:07 +0200

    Add pvs-studio comment to BitArchiveEditor source code

--- src/bitarchiveeditor.cpp
+++ src/bitarchiveeditor.cpp
@@ -1,3 +1,6 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
 /*
  * bit7z - A C++ static library to interface with the 7-zip DLLs.
  * Copyright (c) 2014-2021  Riccardo Ostani - All Rights Reserved.
