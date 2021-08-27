8c4f8ba62a517d66a7fce241f5614a63461cbd7c
Riccardo <rik20@live.it>
2021-08-27 18:07:56 +0200

    Fix compilation with MSVC 2017

--- include/bitinputarchive.hpp
+++ include/bitinputarchive.hpp
@@ -151,11 +151,11 @@ namespace bit7z {
             class const_iterator {
                 public:
                     // iterator traits
-                    using iterator_category BIT7Z_MAYBE_UNUSED = std::input_iterator_tag;
-                    using value_type BIT7Z_MAYBE_UNUSED = BitArchiveItemOffset;
+					using iterator_category = std::input_iterator_tag;
+                    using value_type = BitArchiveItemOffset;
                     using reference = const BitArchiveItemOffset&;
                     using pointer = const BitArchiveItemOffset*;
-                    using difference_type BIT7Z_MAYBE_UNUSED = uint32_t; //so that count_if returns a uint32_t
+                    using difference_type = uint32_t; //so that count_if returns a uint32_t
 
                     const_iterator& operator++() noexcept;
 
