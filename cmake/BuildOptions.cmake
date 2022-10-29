d904a7ba6b70a76329d24f75712f4b1cf2c2433a
Oz <rik20@live.it>
2022-10-29 19:38:31 +0200

    Rename library target name variable in CMake files

--- cmake/BuildOptions.cmake
+++ cmake/BuildOptions.cmake
@@ -7,24 +7,24 @@
 option( BIT7Z_AUTO_FORMAT "Enable or disable auto format detection" )
 message( STATUS "Auto format detection: ${BIT7Z_AUTO_FORMAT}" )
 if( BIT7Z_AUTO_FORMAT )
-    target_compile_definitions( ${TARGET_NAME} PUBLIC BIT7Z_AUTO_FORMAT )
+    target_compile_definitions( ${LIB_TARGET} PUBLIC BIT7Z_AUTO_FORMAT )
 endif()
 
 option( BIT7Z_REGEX_MATCHING "Enable or disable regex matching of archived files" )
 message( STATUS "Regex matching extraction: ${BIT7Z_REGEX_MATCHING}" )
 if( BIT7Z_REGEX_MATCHING )
-    target_compile_definitions( ${TARGET_NAME} PUBLIC BIT7Z_REGEX_MATCHING )
+    target_compile_definitions( ${LIB_TARGET} PUBLIC BIT7Z_REGEX_MATCHING )
 endif()
 
 option( BIT7Z_USE_STD_BYTE "Enable or disable using type safe byte type (like std::byte) for buffers" )
 message( STATUS "Use std::byte: ${BIT7Z_USE_STD_BYTE}" )
 if( BIT7Z_USE_STD_BYTE )
-    target_compile_definitions( ${TARGET_NAME} PUBLIC BIT7Z_USE_STD_BYTE )
+    target_compile_definitions( ${LIB_TARGET} PUBLIC BIT7Z_USE_STD_BYTE )
 endif()
 
 option( BIT7Z_USE_NATIVE_STRING "Enable or disable using the OS native string type
                                  (e.g., std::wstring on Windows, std::string elsewhere)" )
 message( STATUS "Use native string: ${BIT7Z_USE_NATIVE_STRING}" )
 if( BIT7Z_USE_NATIVE_STRING )
-    target_compile_definitions( ${TARGET_NAME} PUBLIC BIT7Z_USE_NATIVE_STRING )
+    target_compile_definitions( ${LIB_TARGET} PUBLIC BIT7Z_USE_NATIVE_STRING )
 endif()
--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -6,18 +6,18 @@
 
 if( MSVC )
     # setting a pdb file name for debug builds (otherwise it is not generated!)
-    set_target_properties( ${TARGET_NAME} PROPERTIES COMPILE_PDB_NAME_DEBUG ${TARGET_NAME}${CMAKE_DEBUG_POSTFIX} )
+    set_target_properties( ${LIB_TARGET} PROPERTIES COMPILE_PDB_NAME_DEBUG ${LIB_TARGET}${CMAKE_DEBUG_POSTFIX} )
 
     # release builds should be optimized (e.g., for size)
-    target_compile_options( ${TARGET_NAME} PRIVATE "$<$<OR:$<CONFIG:RELEASE>,$<CONFIG:MINSIZEREL>>:/Os>" )
-    target_compile_options( ${TARGET_NAME} PRIVATE "$<$<OR:$<CONFIG:RELEASE>,$<CONFIG:MINSIZEREL>>:/Oi>" )
-    target_compile_options( ${TARGET_NAME} PRIVATE "$<$<OR:$<CONFIG:RELEASE>,$<CONFIG:MINSIZEREL>>:/GS>" )
-    target_compile_options( ${TARGET_NAME} PRIVATE "$<$<OR:$<CONFIG:RELEASE>,$<CONFIG:MINSIZEREL>>:/Gy>" )
+    target_compile_options( ${LIB_TARGET} PRIVATE "$<$<OR:$<CONFIG:RELEASE>,$<CONFIG:MINSIZEREL>>:/Os>" )
+    target_compile_options( ${LIB_TARGET} PRIVATE "$<$<OR:$<CONFIG:RELEASE>,$<CONFIG:MINSIZEREL>>:/Oi>" )
+    target_compile_options( ${LIB_TARGET} PRIVATE "$<$<OR:$<CONFIG:RELEASE>,$<CONFIG:MINSIZEREL>>:/GS>" )
+    target_compile_options( ${LIB_TARGET} PRIVATE "$<$<OR:$<CONFIG:RELEASE>,$<CONFIG:MINSIZEREL>>:/Gy>" )
 
     option( BIT7Z_ANALYZE_CODE "Enable or disable code analysis" )
     message( STATUS "Code analysis: ${BIT7Z_ANALYZE_CODE}" )
     if( BIT7Z_ANALYZE_CODE )
-        target_compile_options( ${TARGET_NAME} PRIVATE "$<$<OR:$<CONFIG:RELEASE>,$<CONFIG:MINSIZEREL>>:/analyze>" )
+        target_compile_options( ${LIB_TARGET} PRIVATE "$<$<OR:$<CONFIG:RELEASE>,$<CONFIG:MINSIZEREL>>:/analyze>" )
     endif()
 
     # remove CMake default warning level
@@ -25,16 +25,16 @@ if( MSVC )
     string( REGEX REPLACE "/GR" "/GR-" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" )
 
     # warning flags (as suggested in https://lefticus.gitbooks.io/cpp-best-practices/)
-    target_compile_options( ${TARGET_NAME} PRIVATE /W4 /w14640 /w14242 /w14254 /w14263 /w14265 /w14287 /we4289 /w14296
+    target_compile_options( ${LIB_TARGET} PRIVATE /W4 /w14640 /w14242 /w14254 /w14263 /w14265 /w14287 /we4289 /w14296
                             /w14311 /w14545 /w14546 /w14547 /w14549 /w14555 /w14619 /w14640 /w14826 /w14905 /w14906
                             /w14928 )
 
     # C++ standard conformance options of MSVC
-    target_compile_options( ${TARGET_NAME} PRIVATE /fp:precise /Zc:wchar_t /Zc:rvalueCast /Zc:inline
+    target_compile_options( ${LIB_TARGET} PRIVATE /fp:precise /Zc:wchar_t /Zc:rvalueCast /Zc:inline
                             /Zc:forScope /Zc:strictStrings /Zc:throwingNew /Zc:referenceBinding )
 
     if( CMAKE_GENERATOR MATCHES "Visual Studio" )
-        target_compile_options( ${TARGET_NAME} PRIVATE /MP$ENV{NUMBER_OF_PROCESSORS} )
+        target_compile_options( ${LIB_TARGET} PRIVATE /MP$ENV{NUMBER_OF_PROCESSORS} )
     endif()
 
     # linker flags
@@ -42,12 +42,12 @@ if( MSVC )
 
     # options only for specific MSVC versions
     if( MSVC_VERSION GREATER_EQUAL 1910 ) # MSVC >= 15.0 (VS 2017)
-        target_compile_options( ${TARGET_NAME} PRIVATE /Zc:__cplusplus )
+        target_compile_options( ${LIB_TARGET} PRIVATE /Zc:__cplusplus )
         # treating warnings as errors
-        target_compile_options( ${TARGET_NAME} PRIVATE /WX )
+        target_compile_options( ${LIB_TARGET} PRIVATE /WX )
     else() # MSVC < 15.0 (i.e., <= VS 2015)
         # ignoring C4127 warning
-        target_compile_options( ${TARGET_NAME} PRIVATE /wd4127 )
+        target_compile_options( ${LIB_TARGET} PRIVATE /wd4127 )
     endif()
 
     # static runtime option
@@ -66,16 +66,16 @@ if( MSVC )
         endforeach()
     endif()
 else()
-    target_compile_options( ${TARGET_NAME} PRIVATE -Wall -Wextra -Werror )
+    target_compile_options( ${LIB_TARGET} PRIVATE -Wall -Wextra -Werror )
 endif()
 
 # Extra warning flags for Clang
 if( CMAKE_CXX_COMPILER_ID MATCHES "Clang" )
     if( CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 3.6 )
-        target_compile_options( ${TARGET_NAME} PRIVATE -Wno-inconsistent-missing-override )
+        target_compile_options( ${LIB_TARGET} PRIVATE -Wno-inconsistent-missing-override )
     endif()
     if( CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 3.8 )
-        target_compile_options( ${TARGET_NAME} PRIVATE -Wdouble-promotion )
+        target_compile_options( ${LIB_TARGET} PRIVATE -Wdouble-promotion )
     endif()
 endif()
 
@@ -86,21 +86,21 @@ endif()
 
 # Extra warning flags for GCC
 if( CMAKE_CXX_COMPILER_ID MATCHES "GNU" )
-    target_compile_options( ${TARGET_NAME} PRIVATE -Wshadow -Wcast-align -Wunused
+    target_compile_options( ${LIB_TARGET} PRIVATE -Wshadow -Wcast-align -Wunused
                             -Woverloaded-virtual -Wformat=2 -Wdouble-promotion -Wlogical-op )
     if( NOT MINGW )
-        target_compile_options( ${TARGET_NAME} PRIVATE -Wnon-virtual-dtor )
+        target_compile_options( ${LIB_TARGET} PRIVATE -Wnon-virtual-dtor )
     else()
-        target_compile_options( ${TARGET_NAME} PRIVATE -Wno-error=non-virtual-dtor )
+        target_compile_options( ${LIB_TARGET} PRIVATE -Wno-error=non-virtual-dtor )
         if( CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 8.0 )
-            target_compile_options( ${TARGET_NAME} PRIVATE -Wno-cast-function-type )
+            target_compile_options( ${LIB_TARGET} PRIVATE -Wno-cast-function-type )
         endif()
     endif()
     if( CMAKE_CXX_COMPILER_VERSION VERSION_LESS 5.0 )
-        target_compile_options( ${TARGET_NAME} PRIVATE -Wno-missing-field-initializers )
+        target_compile_options( ${LIB_TARGET} PRIVATE -Wno-missing-field-initializers )
     endif()
     if( CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 6.0 )
-        target_compile_options( ${TARGET_NAME} PRIVATE
+        target_compile_options( ${LIB_TARGET} PRIVATE
                                 # GCC 6.0+ complains on 7-zip macros using misleading indentation,
                                 # disabling the warning to make it compile.
                                 -Wno-misleading-indentation
@@ -109,6 +109,6 @@ if( CMAKE_CXX_COMPILER_ID MATCHES "GNU" )
     endif()
     if( CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 7.0 )
         # Extra warning flags for GCC 7.0+
-        target_compile_options( ${TARGET_NAME} PRIVATE -Wduplicated-branches )
+        target_compile_options( ${LIB_TARGET} PRIVATE -Wduplicated-branches )
     endif()
 endif()
