58f91e588bf9b5ef336f34425cf02426fc3020ef
Oz <rik20@live.it>
2023-01-02 09:59:47 +0100

    Fix warnings in callback.hpp

--- include/internal/callback.hpp
+++ include/internal/callback.hpp
@@ -22,6 +22,18 @@ constexpr auto kEmptyFileAlias = BIT7Z_STRING( "[Content]" );
 
 namespace bit7z {
 
+/* On Windows, 7-zip's CMyUnknownImp class has a virtual destructor if the compiler is MinGW/GCC/Clang.
+ * On MSVC or on Unix, the destructor is not virtual. */
+#ifdef _WIN32
+#   if defined(__GNUC__) || defined(__clang__)
+#       define CALLBACK_DESTRUCTOR( x ) x override
+#   endif
+#endif
+
+#ifndef CALLBACK_DESTRUCTOR // MSVC or Unix compiler
+#   define CALLBACK_DESTRUCTOR( x ) virtual x
+#endif
+
 class Callback : protected CMyUnknownImp {
     public:
         Callback( const Callback& ) = delete;
@@ -32,7 +44,7 @@ class Callback : protected CMyUnknownImp {
 
         Callback& operator=( Callback&& ) = delete;
 
-        virtual ~Callback() = default;
+        CALLBACK_DESTRUCTOR( ~Callback() ) = default;
 
     protected:
         explicit Callback( const BitAbstractArchiveHandler& handler ); // Abstract class
