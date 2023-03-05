7d0a611cc44e43554f4e424f5cbd337c3b216ebe
Oz <rik20@live.it>
2023-03-05 12:44:35 +0100

    Make third_party and 7-zip folders paths configurable by parent projects
    Close issue #126

--- cmake/Filesystem.cmake
+++ cmake/Filesystem.cmake
@@ -26,13 +26,13 @@ endif()
 if( NOT USE_STANDARD_FILESYSTEM OR NOT STANDARD_FILESYSTEM_COMPILES )
     set( CMAKE_CXX_STANDARD 14 ) # if standard filesystem lib is not supported, revert to C++14 standard
 
-    if( NOT EXISTS ${EXTERNAL_LIBS_DIR}/ghc/filesystem.hpp )
+    if( NOT EXISTS ${BIT7Z_EXTERNAL_LIBS_DIR}/ghc/filesystem.hpp )
         include( ExternalProject )
 
         # downloading ghc::filesystem as an alternative to std::filesystem
         file( DOWNLOAD
               https://raw.githubusercontent.com/gulrak/filesystem/master/include/ghc/filesystem.hpp
-              ${EXTERNAL_LIBS_DIR}/ghc/filesystem.hpp
+              ${BIT7Z_EXTERNAL_LIBS_DIR}/ghc/filesystem.hpp
               SHOW_PROGRESS
               TLS_VERIFY ON )
     endif()
