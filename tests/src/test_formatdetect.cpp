0a72a77e960e5560d02c647b495bc793acbf2e14
Oz <rik20@live.it>
2023-06-04 18:36:11 +0200

    Add tests for format detection from signature

--- tests/src/test_formatdetect.cpp
+++ tests/src/test_formatdetect.cpp
@@ -15,7 +15,9 @@
 #include <catch2/catch.hpp>
 
 #include "filesystem.hpp"
+#include "shared_lib.hpp"
 
+#include <bitarchivereader.hpp>
 #include <bitformat.hpp>
 #include <internal/formatdetect.hpp>
 
@@ -117,6 +119,106 @@ TEST_CASE( "formatdetect: Format detection by extension", "[formatdetect]" ) {
 
     DYNAMIC_SECTION( "Extension: " << test.extension ) {
         REQUIRE( detectFormatFromExt( "valid." + test.extension ) == test.format );
+    }
+
+    REQUIRE( set_current_dir( old_current_dir ) );
+}
+
+TEST_CASE( "formatdetect: Format detection by signature", "[formatdetect]" ) {
+    const fs::path old_current_dir = current_dir();
+    const auto test_dir = fs::path{ test_archives_dir } / "detection" / "valid";
+    REQUIRE( set_current_dir( test_dir ) );
+
+    const Bit7zLibrary lib{ test::sevenzip_lib_path() };
+
+    auto test = GENERATE( TestDetection{ "7z", BitFormat::SevenZip },
+                          TestDetection{ "ar", BitFormat::Deb },
+                          TestDetection{ "arj", BitFormat::Arj },
+                          TestDetection{ "bz2", BitFormat::BZip2 },
+                          TestDetection{ "bzip2", BitFormat::BZip2 },
+                          TestDetection{ "cab", BitFormat::Cab },
+                          TestDetection{ "chi", BitFormat::Chm },
+                          TestDetection{ "chm", BitFormat::Chm },
+                          TestDetection{ "cpio", BitFormat::Cpio },
+                          TestDetection{ "cramfs", BitFormat::CramFS },
+                          TestDetection{ "deb", BitFormat::Deb },
+                          TestDetection{ "doc", BitFormat::Compound },
+                          TestDetection{ "docx", BitFormat::Zip },
+                          TestDetection{ "dylib", BitFormat::Macho },
+                          TestDetection{ "elf", BitFormat::Elf },
+                          TestDetection{ "exe", BitFormat::Pe }, // We don't consider SFX executables!
+                          TestDetection{ "ext2", BitFormat::Ext },
+                          TestDetection{ "ext3", BitFormat::Ext },
+                          TestDetection{ "ext4", BitFormat::Ext },
+                          TestDetection{ "ext4.img", BitFormat::Ext },
+                          TestDetection{ "fat", BitFormat::Fat },
+                          TestDetection{ "fat12.img", BitFormat::Fat },
+                          TestDetection{ "fat16.img", BitFormat::Fat },
+                          TestDetection{ "gpt", BitFormat::GPT },
+                          TestDetection{ "gz", BitFormat::GZip },
+                          TestDetection{ "gzip", BitFormat::GZip },
+                          TestDetection{ "hfs", BitFormat::Hfs },
+                          TestDetection{ "iso", BitFormat::Iso },
+                          TestDetection{ "iso.img", BitFormat::Iso },
+                          TestDetection{ "lha", BitFormat::Lzh },
+                          TestDetection{ "lzh", BitFormat::Lzh },
+                          TestDetection{ "lzma", BitFormat::Lzma },
+                          TestDetection{ "lzma86", BitFormat::Lzma86 },
+                          TestDetection{ "macho", BitFormat::Macho },
+                          TestDetection{ "mslz", BitFormat::Mslz },
+                          TestDetection{ "nsis", BitFormat::Nsis },
+                          TestDetection{ "ntfs", BitFormat::Ntfs },
+                          TestDetection{ "ntfs.img", BitFormat::Ntfs },
+                          TestDetection{ "odp", BitFormat::Zip },
+                          TestDetection{ "ods", BitFormat::Zip },
+                          TestDetection{ "odt", BitFormat::Zip },
+                          TestDetection{ "ova", BitFormat::Tar },
+                          TestDetection{ "part1.rar", BitFormat::Rar5 },
+                          TestDetection{ "part2.rar", BitFormat::Rar5 },
+                          TestDetection{ "part3.rar", BitFormat::Rar5 },
+                          TestDetection{ "pkg", BitFormat::Xar },
+                          TestDetection{ "pmd", BitFormat::Ppmd },
+                          TestDetection{ "ppmd", BitFormat::Ppmd },
+                          TestDetection{ "ppt", BitFormat::Compound },
+                          TestDetection{ "pptx", BitFormat::Zip },
+                          TestDetection{ "qcow", BitFormat::QCow },
+                          TestDetection{ "qcow2", BitFormat::QCow },
+                          TestDetection{ "rar4.rar", BitFormat::Rar },
+                          TestDetection{ "rar5.rar", BitFormat::Rar5 },
+                          TestDetection{ "rpm", BitFormat::Rpm },
+                          TestDetection{ "sqsh", BitFormat::SquashFS },
+                          TestDetection{ "squashfs", BitFormat::SquashFS },
+                          TestDetection{ "swf", BitFormat::Swf },
+                          TestDetection{ "swm", BitFormat::Wim },
+                          TestDetection{ "tar", BitFormat::Tar },
+                          TestDetection{ "taz", BitFormat::Z },
+                          TestDetection{ "tbz", BitFormat::BZip2 },
+                          TestDetection{ "tbz2", BitFormat::BZip2 },
+                          TestDetection{ "tgz", BitFormat::GZip },
+                          TestDetection{ "txz", BitFormat::Xz },
+                          TestDetection{ "udf", BitFormat::Udf },
+                          TestDetection{ "udf.img", BitFormat::Udf },
+                          TestDetection{ "udf.iso", BitFormat::Udf },
+                          TestDetection{ "vdi", BitFormat::VDI },
+                          TestDetection{ "vhd", BitFormat::Vhd },
+                          TestDetection{ "vmdk", BitFormat::VMDK },
+                          TestDetection{ "wim", BitFormat::Wim },
+                          TestDetection{ "xar", BitFormat::Xar },
+                          TestDetection{ "xls", BitFormat::Compound },
+                          TestDetection{ "xlsx", BitFormat::Zip },
+                          TestDetection{ "xz", BitFormat::Xz },
+                          TestDetection{ "z", BitFormat::Z },
+                          TestDetection{ "zip", BitFormat::Zip },
+                          TestDetection{ "zipx", BitFormat::Zip } );
+
+    DYNAMIC_SECTION( "Extension: " << test.extension ) {
+        // We might directly test the detectFormatFromSig function, but it would require us to include
+        // too many internal headers, and it easily gives compilation problems.
+        // Hence, we use BitArchiveReader for reading the file from a buffer (to avoid signature detection).
+
+        const auto file_buffer = load_file( "valid." + test.extension );
+        const BitArchiveReader reader{ lib, file_buffer };
+        REQUIRE( reader.detectedFormat() == test.format );
     }
 
     REQUIRE( set_current_dir( old_current_dir ) );
