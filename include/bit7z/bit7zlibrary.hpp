d2d4976a90518e5f98c1f5140ce3e0df59e5d877
Oz <rik20@live.it>
2023-07-15 09:36:21 +0200

    Update copyright comment

--- include/bit7z/bit7zlibrary.hpp
+++ include/bit7z/bit7zlibrary.hpp
@@ -1,6 +1,6 @@
 /*
  * bit7z - A C++ static library to interface with the 7-zip shared libraries.
- * Copyright (c) 2014-2022 Riccardo Ostani - All Rights Reserved.
+ * Copyright (c) 2014-2023 Riccardo Ostani - All Rights Reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
