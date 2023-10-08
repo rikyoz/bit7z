d65a161748d49ad24af7ae6430500fa8a7f402d6
Oz <rik20@live.it>
2023-10-08 17:33:14 +0200

    Add placeholder tests for missing unit tests

---
+++ tests/src/test_bitfileextractor.cpp
@@ -0,0 +1,26 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+/*
+ * bit7z - A C++ static library to interface with the 7-zip shared libraries.
+ * Copyright (c) 2014-2023 Riccardo Ostani - All Rights Reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include <catch2/catch.hpp>
+
+#include "utils/shared_lib.hpp"
+
+#include <bit7z/bitfileextractor.hpp>
+
+using namespace bit7z;
+
+TEST_CASE( "BitFileExtractor: TODO", "[bitfileextractor]" ) {
+    const Bit7zLibrary lib{ test::sevenzip_lib_path() };
+
+    const BitFileExtractor extractor{lib, BitFormat::SevenZip};
+    REQUIRE( extractor.extractionFormat() == BitFormat::SevenZip ); // Just a placeholder test.
+}
