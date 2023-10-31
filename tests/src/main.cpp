beb61564cb80d80e9cd1d979b2185f9bfc99fcfc
Oz <rik20@live.it>
2023-10-31 18:32:11 +0100

    [Test] Fix running tests with CTest

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -24,40 +24,40 @@
 auto main( int argc, char* argv[] ) -> int try {
     using namespace bit7z::test;
 
-    std::cout << "[Compiler]\n";
-    std::cout << "Name: " << compiler::name << '\n';
-    std::cout << "Version: " << compiler::version << '\n';
-    std::cout << "Target Architecture: " << compiler::target_arch << "\n\n";
+    std::clog << "[Compiler]\n";
+    std::clog << "Name: " << compiler::name << '\n';
+    std::clog << "Version: " << compiler::version << '\n';
+    std::clog << "Target Architecture: " << compiler::target_arch << "\n\n";
 
-    std::cout << "[C Runtime Library]\n";
-    std::cout << "Name: " << compiler::c_runtime << "\n\n";
+    std::clog << "[C Runtime Library]\n";
+    std::clog << "Name: " << compiler::c_runtime << "\n\n";
 
-    std::cout << "[C++ Standard Library]\n";
-    std::cout << "Name: " << compiler::standard_library << '\n';
-    std::cout << "Version: " << compiler::standard_library_version << "\n\n";
+    std::clog << "[C++ Standard Library]\n";
+    std::clog << "Name: " << compiler::standard_library << '\n';
+    std::clog << "Version: " << compiler::standard_library_version << "\n\n";
 
-    std::cout << "[Runtime]\n";
-    std::cout << "Executable path: " << filesystem::exe_path().string() << '\n';
-    std::cout << "7-zip shared library: ";
+    std::clog << "[Runtime]\n";
+    std::clog << "Executable path: " << filesystem::exe_path().string() << '\n';
+    std::clog << "7-zip shared library: ";
 #if defined( BIT7Z_USE_NATIVE_STRING ) && defined( _WIN32 )
     std::wcout << sevenzip_lib_path() << "\n";
 #else
-    std::cout << sevenzip_lib_path() << '\n';
+    std::clog << sevenzip_lib_path() << '\n';
 #endif
 #ifdef BIT7Z_TESTS_FILESYSTEM
-    std::cout << "Test data path: " << filesystem::test_data_dir << '\n';
+    std::clog << "Test data path: " << filesystem::test_data_dir << '\n';
 #endif
 #ifdef _WIN32
-    std::cout << "Code page: " << GetACP() << '\n';
+    std::clog << "Code page: " << GetACP() << '\n';
 #endif
-    std::cout << '\n';
+    std::clog << '\n';
 
-    std::cout << "[Flags]\n";
-    std::cout << "BIT7Z_AUTO_FORMAT: " << flags::auto_format << '\n';
-    std::cout << "BIT7Z_REGEX_MATCHING: " << flags::regex_matching << '\n';
-    std::cout << "BIT7Z_USE_NATIVE_STRING: " << flags::native_string << '\n';
-    std::cout << "BIT7Z_USE_STANDARD_FILESYSTEM: " << flags::standard_filesystem << "\n\n";
-    std::cout.flush();
+    std::clog << "[Flags]\n";
+    std::clog << "BIT7Z_AUTO_FORMAT: " << flags::auto_format << '\n';
+    std::clog << "BIT7Z_REGEX_MATCHING: " << flags::regex_matching << '\n';
+    std::clog << "BIT7Z_USE_NATIVE_STRING: " << flags::native_string << '\n';
+    std::clog << "BIT7Z_USE_STANDARD_FILESYSTEM: " << flags::standard_filesystem << "\n\n";
+    std::clog.flush();
 
     return Catch::Session().run( argc, argv );
 } catch( ... ) {
