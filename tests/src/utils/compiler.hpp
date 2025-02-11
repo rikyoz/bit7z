aa4def0f43f786b2b77c58617bcedb0f143bc18d
Oz <rik20@live.it>
2025-02-11 21:06:54 +0100

    Fix test app not reporting the architecture on Arm-based systems

--- tests/src/utils/compiler.hpp
+++ tests/src/utils/compiler.hpp
@@ -92,6 +92,10 @@ constexpr auto c_runtime = "Unknown";
 constexpr auto target_arch = "x64";
 #elif defined( _WIN32 ) || defined( __i386__ )
 constexpr auto target_arch = "x86";
+#elif defined( __aarch64__ )
+constexpr auto target_arch = "arm64"
+#elif defined( __arm__ )
+constexpr auto target_arch = "arm"
 #else
 constexpr auto target_arch = "Unsupported Architecture";
 #endif
