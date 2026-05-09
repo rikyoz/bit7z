bce1a896d8a58347ee5eb0ff79bd5156558c8ebf
Oz <rik20@live.it>
2026-05-09 13:03:53 +0200

    Add Doxygen comments for BitInputItem methods

--- include/bit7z/bitinputitem.hpp
+++ include/bit7z/bitinputitem.hpp
@@ -57,6 +57,9 @@ struct InputItemProperties final {
     InputItemType inputType;
 };
 
+/**
+ * A generic input item for compression operations.
+ */
 class BitInputItem final {
     public:
         explicit BitInputItem( const fs::path& itemPath, SymlinkPolicy symlinkPolicy = SymlinkPolicy::Follow );
@@ -73,37 +76,88 @@ class BitInputItem final {
 
         BitInputItem( const BitInputArchive& inputArchive, std::uint32_t index, const tstring& newPath );
 
+        /**
+         * @return true if the item represents a directory.
+         */
         BIT7Z_NODISCARD
         auto isDir() const noexcept -> bool;
 
+        /**
+         * @return true if the item represents a symbolic link.
+         */
         BIT7Z_NODISCARD
         auto isSymLink() const noexcept -> bool;
 
+        /**
+         * @return the uncompressed size of the item, in bytes.
+         */
         BIT7Z_NODISCARD
         auto size() const noexcept -> std::uint64_t;
 
+        /**
+         * @return the native filesystem path of the item.
+         */
         BIT7Z_NODISCARD
         auto path() const -> const native_string&;
 
+        /**
+         * @return the filesystem attributes of the item.
+         */
         BIT7Z_NODISCARD
         auto attributes() const noexcept -> std::uint32_t;
 
+        /**
+         * @return the path of the item as it will be stored inside the archive.
+         */
         BIT7Z_NODISCARD
         auto inArchivePath() const -> const sevenzip_string&;
 
+        /**
+         * @param property the property to retrieve.
+         *
+         * @return the value of the given property for this item.
+         */
         BIT7Z_NODISCARD
         auto itemProperty( BitProperty property ) const -> BitPropVariant;
 
+        /**
+         * @brief Provides the input stream for this item to be used during compression.
+         *
+         * @note For internal use; not meant to be used by the user.
+         *
+         * @param inStream output parameter set to the item's input stream.
+         *
+         * @return S_OK on success, or an error HRESULT if the stream could not be opened.
+         */
         BIT7Z_NODISCARD
         auto getStream( ISequentialInStream** inStream ) const -> HRESULT;
 
+        /**
+         * @return true if the item contributes new data to the archive
+         *         (i.e., it is not merely a renamed entry from an existing archive).
+         */
         BIT7Z_NODISCARD
         auto hasNewData() const noexcept -> bool;
 
+        /**
+         * @brief Sets the creation time of the item.
+         *
+         * @param creationTime the creation time to set.
+         */
         void setCreationTime( time_type creationTime ) noexcept;
 
+        /**
+         * @brief Sets the last write time of the item.
+         *
+         * @param lastWriteTime the last write time to set.
+         */
         void setLastWriteTime( time_type lastWriteTime ) noexcept;
 
+        /**
+         * @brief Sets the last access time of the item.
+         *
+         * @param lastAccessTime the last access time to set.
+         */
         void setLastAccessTime( time_type lastAccessTime ) noexcept;
 
     private:
